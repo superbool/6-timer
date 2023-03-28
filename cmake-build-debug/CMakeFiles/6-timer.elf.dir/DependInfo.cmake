57720d4724cc92d64b092d52110922568220577c
kefei <kefei@jzps.com>
2023-03-28 14:20:27 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/6-timer.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/study_test/6-timer/Core/Startup/startup_stm32f407zgtx.s" "D:/study_test/6-timer/cmake-build-debug/CMakeFiles/6-timer.elf.dir/Core/Startup/startup_stm32f407zgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/study_test/6-timer/Core/Inc"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/study_test/6-timer/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/study_test/6-timer/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/study_test/6-timer/Core/Src/gpio.c" "CMakeFiles/6-timer.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/main.c" "CMakeFiles/6-timer.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/main.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/6-timer.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/stm32f4xx_it.c" "CMakeFiles/6-timer.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/syscalls.c" "CMakeFiles/6-timer.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/sysmem.c" "CMakeFiles/6-timer.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/system_stm32f4xx.c" "CMakeFiles/6-timer.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/tim.c" "CMakeFiles/6-timer.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/tim.c.obj.d"
+  "D:/study_test/6-timer/Core/Src/usart.c" "CMakeFiles/6-timer.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "D:/study_test/6-timer/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/6-timer.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
